9ab3e657c3a383eb9ba0500285ba19bcf32938f9
Mo David <modavid.1964@gmail.com>
2024-10-03 21:48:50 +0800

    Made sure Windows script performed cleanup too.

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 forfiles /s /m "*.java" /c "cmd /c echo @relpath" > sources.txt
 powershell -Command "(gc sources.txt) -replace '\\', '\\\\' | Out-File -encoding ASCII sources.txt"
 javac @sources.txt -d out/ -cp out
+del /s sources.txt
 java -classpath out main.Driver %*
